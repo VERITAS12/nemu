47a1be742d299163f6d141bd836aacb52397b266
tracer-ics2016 <tracer@njuics.org>
2020-09-25 14:16:31 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:16:31 up  5:11,  5 users,  load average: 0.28, 0.18, 0.18 5b73a25f80214c02523fd82fd77f5ef8647c6be1

---
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -0,0 +1,24 @@
+#include "cpu/exec/template-start.h"
+
+#define instr sub
+
+static void do_execute(){
+	DATA_TYPE result = op_dest->val - op_src->val;
+	int len = (DATA_BYTE << 3) - 1;
+	int i=0, cnt=0;
+	for(;i<8;i++)if(((1<<i)&result)!=0)cnt++;
+	cpu.PF = !(cnt%2);
+	cpu.ZF = !result;
+	cpu.SF = result >> len;
+	cpu.OF = (op_src->val>>len)!=(op_dest->val>>len)&&(cpu.SF==(op_src->val>>len));
+	print_asm(str(instr)" %s, %s", op_src->str, op_dest->str);
+}
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+#include "cpu/exec/template-end.h"
