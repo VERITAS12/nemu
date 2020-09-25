512ec7a80cd99e18b63f83316a69124ffca2e875
tracer-ics2016 <tracer@njuics.org>
2020-09-25 12:15:18 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:15:18 up  3:10,  5 users,  load average: 0.02, 0.06, 0.01 302f4b7995611894da36df088ed2aee18d784608

---
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -0,0 +1,27 @@
+#include "cpu/exec/template-start.h"
+
+#define instr cmp
+
+static void do_execute(){
+	DATA_TYPE result = op_dest->val - op_src->val;
+	int i=0, cnt = 0;
+	for(;i<8;i++)if(((1<<i)&result)!=0)cnt++;
+	cpu.PF = !(cnt%2);
+	cpu.ZF = !result;
+	cpu.SF = result>>((DATA_BYTE<<3)-1);
+	int len = (DATA_BYTE<<3)-1;
+	cpu.OF = (op_dest->val>>len)!=(op_src->val>>len)&&(op_src->val>>len)==cpu.SF;
+	print_asm(str(instr)" %s, %s", op_src->str, op_dest->str);
+}
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+
+
+#include "cpu/exec/template-end.h"
