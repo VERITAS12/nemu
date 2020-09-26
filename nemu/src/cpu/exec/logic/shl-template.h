173d156f88e4bdba7dbda2ee90daf1567819aa48
tracer-ics2016 <tracer@njuics.org>
2020-09-26 05:48:01 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:48:01 up 20:43,  5 users,  load average: 0.13, 0.19, 0.17 e2a67c0bb7f00ebe3994173d7387d15d163eddb2

--- nemu/src/cpu/exec/logic/shl-template.h
+++ nemu/src/cpu/exec/logic/shl-template.h
@@ -5,14 +5,21 @@
 static void do_execute () {
 	DATA_TYPE src = op_src->val;
 	DATA_TYPE dest = op_dest->val;
-
+	DATA_TYPE tmp = op_dest->val;
 	uint8_t count = src & 0x1f;
 	dest <<= count;
 	OPERAND_W(op_dest, dest);
 
 	/* TODO: Update EFLAGS. */
-	panic("please implement me");
-
+//	panic("please implement me");
+	cpu.OF = (op_src->val>>((DATA_BYTE <<3 ) - 1))?1:0;
+	cpu.CF = 0;
+	cpu.ZF = !tmp;
+	cpu.SF = tmp>>((DATA_BYTE <<3 ) - 1);
+	int i=0,cnt=0;
+	for(;i<8;i++)if(((1<<i)&tmp)!=0)cnt++;
+	cpu.PF = !(cnt%2);
+	
 	print_asm_template2();
 }
 
