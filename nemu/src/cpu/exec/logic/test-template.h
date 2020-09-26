edeafab94a4ef82b05effa98a34b7420d1ff07e0
tracer-ics2016 <tracer@njuics.org>
2020-09-26 14:58:01 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:58:01 up 1 day,  5:53, 11 users,  load average: 0.60, 0.42, 0.50 d2a9016260358b417613f01181388c4beb524f1c

--- nemu/src/cpu/exec/logic/test-template.h
+++ nemu/src/cpu/exec/logic/test-template.h
@@ -6,9 +6,9 @@ static void do_execute(){
 	DATA_TYPE result = op_dest->val & op_src->val;
 	cpu.CF = 0;
 	cpu.ZF = !result;
-	// printf("test: --------------\n");
-	// printf("val:  %dval2:  %d\n", op_dest->val, op_src->val);
-	// printf("ZF:      %d\n", cpu.ZF);
+	printf("test: --------------\n");
+	printf("val:  %dval2:  %d\n", op_dest->val, op_src->val);
+	printf("ZF:      %d\n", cpu.ZF);
 	cpu.SF = result >> ((DATA_BYTE << 3) - 1);
 	int i;
 	int cnt = 0;
