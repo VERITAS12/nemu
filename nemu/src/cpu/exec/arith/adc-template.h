182583422f1c487902b25ff014365ad7f7a1f20d
tracer-ics2016 <tracer@njuics.org>
2020-09-26 14:27:20 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:27:20 up 1 day,  5:22,  5 users,  load average: 0.43, 0.41, 0.44 4732b4f3454b897f7836ff0beef51168e8a3ed77

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -3,6 +3,7 @@
 #define instr adc
 static void do_execute(){
 	DATA_TYPE result = op_dest->val + op_src->val  + cpu.ZF;
+	printf("adc:  %d\n", result);
 	int len = (DATA_BYTE<<3)-1;
 	cpu.CF = result < op_dest->val;
 	cpu.SF = result>>len;
