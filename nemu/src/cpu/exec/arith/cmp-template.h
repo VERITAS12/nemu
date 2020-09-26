4e4c1f985c556729c46ad191e09e35fd10b11188
tracer-ics2016 <tracer@njuics.org>
2020-09-26 15:00:11 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:00:11 up 1 day,  5:55, 11 users,  load average: 1.14, 0.66, 0.57 9a3ceaeeabe1b39447480787f6ba2ec4326fd884

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -4,10 +4,14 @@
 
 static void do_execute(){
 	DATA_TYPE result = op_dest->val - op_src->val;
+
 	int i=0, cnt = 0;
 	for(;i<8;i++)if(((1<<i)&result)!=0)cnt++;
 	cpu.PF = !(cnt%2);
 	cpu.ZF = !result;
+	printf("cmp: --------------\n");
+	printf("val:  %dval2:  %d\n", op_dest->val, op_src->val);
+	printf("ZF:      %d\n", cpu.ZF);
 	cpu.SF = result>>((DATA_BYTE<<3)-1);
 	int len = (DATA_BYTE<<3)-1;
 	cpu.OF = (op_dest->val>>len)!=(op_src->val>>len)&&(op_src->val>>len)==cpu.SF;
