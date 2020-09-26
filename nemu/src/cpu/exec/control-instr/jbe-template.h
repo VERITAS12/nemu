3603debda7f9bdd2829706430405fc4133a86e2b
tracer-ics2016 <tracer@njuics.org>
2020-09-26 03:22:39 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  03:22:39 up 18:17,  5 users,  load average: 0.17, 0.13, 0.10 4078723e288ef20dfde2f1321d8e894f9eb02d96

---
+++ nemu/src/cpu/exec/control-instr/jbe-template.h
@@ -0,0 +1,12 @@
+#include "cpu/exec/template-start.h"
+
+#define instr jbe
+
+static void do_execute(){
+	DATA_TYPE_S disp = op_src->val;
+	print_asm("jbe %x", cpu.eip + 1 + disp);
+	if (cpu.ZF == 1 || cpu.CF == 1)cpu.eip += disp;
+}
+make_instr_helper(i)
+
+#include "cpu/exec/template-end.h"
