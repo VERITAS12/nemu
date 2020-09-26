676e2fc0f66310e53aab07685f6c7b739a8b3b55
tracer-ics2016 <tracer@njuics.org>
2020-09-26 12:04:16 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:04:16 up 1 day,  2:59,  5 users,  load average: 0.25, 0.24, 0.20 11a571b13da352b85de0ebc2a351f1906f70a64a

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -62,6 +62,8 @@ void cpu_exec(volatile uint32_t n) {
 		 * instruction decode, and the actual execution. */
 		int instr_len = exec(cpu.eip);
 		printf("%d\n", instr_len);
+		
+		printf("%d\n", cpu.eip);
 		cpu.eip += instr_len;
 
 #ifdef DEBUG
