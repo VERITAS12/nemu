d1d9462b29c45910f45295dbadc29d3f0b963e32
tracer-ics2016 <tracer@njuics.org>
2020-09-27 14:16:09 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:16:09 up 2 days,  5:11, 11 users,  load average: 0.81, 0.86, 0.83 9b0f9910acc3f0c14782ccb39015eb9522271a19

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -49,6 +49,7 @@
 #include "string/movs.h"
 #include "string/scas.h"
 #include "string/lods.h"
+#include "string/stos.h"
 
 #include "misc/misc.h"
 
---
+++ nemu/src/cpu/exec/string/stos.h
@@ -0,0 +1,7 @@
+#ifndef __STOS_H__
+#define __STOS_H__
+
+make_helper(stos_1_b);
+make_helper(stos_1_v);
+
+#endif
