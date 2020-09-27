4eef3c0deb2def68339b29b805870b78d7206ea6
tracer-ics2016 <tracer@njuics.org>
2020-09-27 13:46:20 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:46:20 up 2 days,  4:41, 11 users,  load average: 0.29, 0.66, 0.67 4e5d2d3b92e404ecfb10223a4c386b1a66961ad

---
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -0,0 +1,30 @@
+#include "cpu/exec/template-start.h"
+#define instr scas
+
+make_helper(concat(scas_l_, SUFFIX)){
+	int val1, val2;
+	if(DATA_BYTE == 2){
+		val1 = swaddr_read(reg_w(R_DI), 2);
+		val2 = reg_w(R_AX);
+		if(cpu.DF==0)reg_w(R_DI)+=DATA_BYTE;
+		else reg_w(R_DI)-=DATA_BYTE;
+	}else{
+		val1 = swaddr_read(reg_w(R_EDI), 2);
+		val2 = reg_w(R_EAX);
+		if(DATA_BYTE==1){val1&=0xff;val2&=0xff;}
+		if(cpu.DF==0)reg_w(R_EDI)+=DATA_BYTE;
+		else reg_w(R_EDI)-=DATA_BYTE;
+	}
+	print_asm_template2();
+	int result = val2-val1;
+	cpu.ZF = !result;
+	cpu.SF=result<0;
+	int len = (DATA_BYTE<<3)-1;
+	cpu.OF = ((op_dest->val>>len)==(op_src->val>>len))&&((op_dest->val>>len)!=cpu.SF);
+	int i = 0, cnt = 0;
+	for(;i<8;i++)if(((1<<i)&result)!=0)cnt++;
+	cpu.PF = !(cnt%2);
+	cpu.CF = (unsigned int)val2 < (unsigned int)val1;
+	return 1;
+}
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/string/scas.h
@@ -0,0 +1,7 @@
+#ifndef __SCAS_H__
+#define __SCAS_H__
+
+make_helper(scas_l_b);
+make_helper(scas_l_v);
+
+#endif
