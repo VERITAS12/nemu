6a80deba22b6b34091cd22ef6f9d4049dc3b4a3c
tracer-ics2016 <tracer@njuics.org>
2020-09-27 12:32:57 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:32:57 up 2 days,  3:28, 11 users,  load average: 0.64, 0.63, 0.58 9844980251ce323c38a33215bd542f00974e1edd

---
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -0,0 +1,31 @@
+#include "cpu/exec/template-start.h"
+
+#define instr sbb
+
+static void do_execute(){
+	int val1 = (int)op_src->val;
+	val1 += cpu.CF;
+	int val2 = op_dest->val;
+	int result = val2 - val1;
+	cpu.ZF = !result;
+	cpu.SF = result < 0?1:0;
+	DATA_TYPE tmp = op_dest->val - cpu.CF - op_src->val;
+	int cnt = 0;
+	while(tmp){
+		tmp &=(tmp-1);
+		cnt++;
+	}
+	cpu.PF = !(cnt%2);
+	if((result>0&&val1>0&&val2<=0)||(result<0&&val1<0&&val2>=0))cpu.OF=1;
+	else cpu.OF=0;
+	cpu.CF = (unsigned int)val2 < (unsigned int)val1;
+	OPERAND_W(op_dest, result);
+
+}
+#if DATA_BYTE==2||DATA_BYTE==4
+make_instr_helper(si2rm)
+#endif
+make_instr_helper(rm2r)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+#include "cpu/exec/template-end.h"
