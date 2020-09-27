a4f107039fc447a31455569d4e0d9ca9955d28c6
tracer-ics2016 <tracer@njuics.org>
2020-09-27 15:54:09 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:54:09 up 2 days,  6:49, 11 users,  load average: 1.44, 0.85, 0.49 8307387ae4f75c9f7f60664c452650dd8901f6f

--- nemu/src/cpu/exec/arith/dec-template.h
+++ nemu/src/cpu/exec/arith/dec-template.h
@@ -1,3 +1,30 @@
+// #include "cpu/exec/template-start.h"
+
+// #define instr dec
+
+// static void do_execute () {
+// 	DATA_TYPE result = op_src->val - 1;
+// 	OPERAND_W(op_src, result);
+
+// 	/* TODO: Update EFLAGS. */
+// 	//panic("please implement me");
+// 	int val1=1, val2 = op_src->val;
+// 	int ans = (int)result;
+// 	cpu.ZF = !result;
+// 	cpu.SF = result>>((DATA_BYTE<<3)-1);
+// 	int i=0,cnt=0;
+// 	for(;i<8;i++)if(((1<<i)&result))cnt++;
+// 	cpu.PF = !(cnt%2);
+// 	cpu.OF = (ans>0&&val1>0&&val2<=0)||(ans<0&&val1<0&&val2>=0);
+// 	print_asm_template1();
+// }
+
+// make_instr_helper(rm)
+// #if DATA_BYTE == 2 || DATA_BYTE == 4
+// make_instr_helper(r)
+// #endif
+
+// #include "cpu/exec/template-end.h"
 #include "cpu/exec/template-start.h"
 
 #define instr dec
@@ -7,15 +34,20 @@ static void do_execute () {
 	OPERAND_W(op_src, result);
 
 	/* TODO: Update EFLAGS. */
-	//panic("please implement me");
-	int val1=1, val2 = op_src->val;
+	int d1 = 1;
+	int d2 = op_src->val;
 	int ans = (int)result;
-	cpu.ZF = !result;
-	cpu.SF = result>>((DATA_BYTE<<3)-1);
-	int i=0,cnt=0;
-	for(;i<8;i++)if(((1<<i)&result))cnt++;
-	cpu.PF = !(cnt%2);
-	cpu.OF = (ans>0&&val1>0&&val2<=0)||(ans<0&&val1<0&&val2>=0);
+	cpu.ZF = !ans;
+	cpu.SF = ans<0?1:0;
+	int cnt =0;
+	while(result) {
+		result = result&(result-1);
+		cnt++;
+	}	
+	cpu.PF = cnt%2==0? 1:0;
+	if((ans>0&&d1>0&&d2<=0)||(ans<0&&d1<0&&d2>=0)) cpu.OF = 1;
+	else cpu.OF = 0;
+
 	print_asm_template1();
 }
 
