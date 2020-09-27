880e17c127c25d40d616b885ebba1706cb3d813d
tracer-ics2016 <tracer@njuics.org>
2020-09-27 14:56:47 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:56:47 up 2 days,  5:52, 11 users,  load average: 0.89, 0.57, 0.54 68994c07b30ccaec41b625e64e83c7ab7d94cf3

---
+++ nemu/src/cpu/exec/control-instr/jns-template.h
@@ -0,0 +1,11 @@
+#include "cpu/exec/template-start.h"
+
+#define instr jns
+
+static void do_execute(){
+	DATA_TYPE_S disp = op_src->val;
+	if(cpu.SF==0)cpu.eip+=disp;
+	print_asm_template1();
+}
+make_instr_helper(i)
+#include "cpu/exec/template-end.h"
