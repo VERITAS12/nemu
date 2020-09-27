4472408aca3540ead45122c974e73a70a979d540
tracer-ics2016 <tracer@njuics.org>
2020-09-27 12:47:38 +0000

    >  run 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:47:38 up 2 days,  3:42, 11 users,  load average: 1.05, 0.81, 0.69 d576a1535b05fd16a2111654837ebe2c38d400

--- testcase/src/sub-longlong.c
+++ testcase/src/sub-longlong.c
@@ -1,9 +1,9 @@
 #include "trap.h"
 
-long long sub(long long a, long long b) {
-	long long c = a - b;
-	return c;
-}
+// long long sub(long long a, long long b) {
+// 	long long c = a - b;
+// 	return c;
+// }
 
 int test_data[] = {0, 1};//, 2, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0x8000000000000001LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL};
 int ans[] = {0LL, -1};//, 0xffffffffffffffffLL, 0xfffffffffffffffeLL, 0x8000000000000001LL, 0x8000000000000000LL, 0x7fffffffffffffffLL, 0x2LL, 0x1LL, 0x1LL, 0LL, 0xffffffffffffffffLL, 0x8000000000000002LL, 0x8000000000000001LL, 0x8000000000000000LL, 0x3LL, 0x2LL, 0x2LL, 0x1LL, 0LL, 0x8000000000000003LL, 0x8000000000000002LL, 0x8000000000000001LL, 0x4LL, 0x3LL, 0x7fffffffffffffffLL, 0x7ffffffffffffffeLL, 0x7ffffffffffffffdLL, 0LL, 0xffffffffffffffffLL, 0xfffffffffffffffeLL, 0x8000000000000001LL, 0x8000000000000000LL, 0x8000000000000000LL, 0x7fffffffffffffffLL, 0x7ffffffffffffffeLL, 0x1LL, 0LL, 0xffffffffffffffffLL, 0x8000000000000002LL, 0x8000000000000001LL, 0x8000000000000001LL, 0x8000000000000000LL, 0x7fffffffffffffffLL, 0x2LL, 0x1LL, 0LL, 0x8000000000000003LL, 0x8000000000000002LL, 0xfffffffffffffffeLL, 0xfffffffffffffffdLL, 0xfffffffffffffffcLL, 0x7fffffffffffffffLL, 0x7ffffffffffffffeLL, 0x7ffffffffffffffdLL, 0LL, 0xffffffffffffffffLL, 0xffffffffffffffffLL, 0xfffffffffffffffeLL, 0xfffffffffffffffdLL, 0x8000000000000000LL, 0x7fffffffffffffffLL, 0x7ffffffffffffffeLL, 0x1LL, 0LL};
@@ -11,7 +11,7 @@ int ans[] = {0LL, -1};//, 0xffffffffffffffffLL, 0xfffffffffffffffeLL, 0x80000000
 #define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
 
 int main() {
-	nemu_assert(sub(test_data[0], test_data[1]) == ans[1]);
+	nemu_assert(test_data[0]-test_data[1] == ans[1]);
 	// int i, j, ans_idx = 0;
 	// int loop = 0;
 	// for(i = 0; i < NR_DATA; i ++) {
