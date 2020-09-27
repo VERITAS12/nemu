e1937bf0a6627c33c6eab04a59f60be447158fcd
tracer-ics2016 <tracer@njuics.org>
2020-09-27 12:48:36 +0000

    >  run 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:48:36 up 2 days,  3:43, 11 users,  load average: 1.21, 0.88, 0.72 1102303c17f0cb83ce9b239a20cd41ccfc4a0e9

--- testcase/src/sub-longlong.c
+++ testcase/src/sub-longlong.c
@@ -4,14 +4,17 @@
 // 	long long c = a - b;
 // 	return c;
 // }
-
+int sub(int a, int b) {
+	int c = a - b;
+	return c;
+}
 int test_data[] = {0, 1};//, 2, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0x8000000000000001LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL};
 int ans[] = {0LL, -1};//, 0xffffffffffffffffLL, 0xfffffffffffffffeLL, 0x8000000000000001LL, 0x8000000000000000LL, 0x7fffffffffffffffLL, 0x2LL, 0x1LL, 0x1LL, 0LL, 0xffffffffffffffffLL, 0x8000000000000002LL, 0x8000000000000001LL, 0x8000000000000000LL, 0x3LL, 0x2LL, 0x2LL, 0x1LL, 0LL, 0x8000000000000003LL, 0x8000000000000002LL, 0x8000000000000001LL, 0x4LL, 0x3LL, 0x7fffffffffffffffLL, 0x7ffffffffffffffeLL, 0x7ffffffffffffffdLL, 0LL, 0xffffffffffffffffLL, 0xfffffffffffffffeLL, 0x8000000000000001LL, 0x8000000000000000LL, 0x8000000000000000LL, 0x7fffffffffffffffLL, 0x7ffffffffffffffeLL, 0x1LL, 0LL, 0xffffffffffffffffLL, 0x8000000000000002LL, 0x8000000000000001LL, 0x8000000000000001LL, 0x8000000000000000LL, 0x7fffffffffffffffLL, 0x2LL, 0x1LL, 0LL, 0x8000000000000003LL, 0x8000000000000002LL, 0xfffffffffffffffeLL, 0xfffffffffffffffdLL, 0xfffffffffffffffcLL, 0x7fffffffffffffffLL, 0x7ffffffffffffffeLL, 0x7ffffffffffffffdLL, 0LL, 0xffffffffffffffffLL, 0xffffffffffffffffLL, 0xfffffffffffffffeLL, 0xfffffffffffffffdLL, 0x8000000000000000LL, 0x7fffffffffffffffLL, 0x7ffffffffffffffeLL, 0x1LL, 0LL};
 
 #define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
 
 int main() {
-	nemu_assert(test_data[0]-test_data[1] == ans[1]);
+	nemu_assert(sub(test_data[0], test_data[1]) == ans[1]);
 	// int i, j, ans_idx = 0;
 	// int loop = 0;
 	// for(i = 0; i < NR_DATA; i ++) {
