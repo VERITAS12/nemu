c6a1573a6557fc1a9af8a305f99433424a6e0ee1
tracer-ics2016 <tracer@njuics.org>
2020-09-27 15:52:58 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:52:58 up 2 days,  6:48, 11 users,  load average: 0.52, 0.60, 0.39 b8e0f90ca9794309996a62c0d9d9a678a980f345

--- nemu/src/cpu/exec/control-instr/ja-template.h
+++ nemu/src/cpu/exec/control-instr/ja-template.h
@@ -3,8 +3,8 @@
 #define instr ja
 
 static void do_execute(){
-	DATA_TYPE_S d = op_src->val;
-	if(cpu.ZF==0&&cpu.CF==0)cpu.eip+=d;
+	DATA_TYPE_S disp = op_src->val;
+	if(cpu.ZF==0&&cpu.CF==0)cpu.eip+=disp;
 	print_asm_template1();
 }
 make_instr_helper(i);
