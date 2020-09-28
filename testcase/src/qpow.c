52919d3bf5a9f10e4cfc8c0729544a05ffb553a9
tracer-ics2016 <tracer@njuics.org>
2020-09-28 14:39:25 +0000

    >  run 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:39:25 up 3 days,  5:34, 11 users,  load average: 0.65, 0.55, 0.49 d173244ecb4b3f9d75c1320ca3e884ef7d4e40e

--- testcase/src/qpow.c
+++ testcase/src/qpow.c
@@ -3,11 +3,15 @@
 long long qpow(long long a, long long n, long long mod){
 	long long re = 1;
 	while(n){
-		if(n&1)re=(re*a)-mod;
+		if(n&1){
+			re=(re*a);
+			while(re>=mod)re-=mod;
+		}
 		n>>=1;
-		a=(a*a)-mod;
-	}
-	return re - mod;
+		a=(a*a);
+		while(a>=mod)a-=mod;
+	}while(re>=mod)re-=mod;
+	return re;
 }
 int main(){
 	/* code */
