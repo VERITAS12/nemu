950a9c87d67be309f93bc1af79d04a4388e6dd5e
tracer-ics2016 <tracer@njuics.org>
2020-09-28 13:47:47 +0000

    >  run 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:47:47 up 3 days,  4:43, 11 users,  load average: 0.68, 0.69, 0.72 748b6d284c6a982a29dad255703b5d93572b0f1d

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -1,26 +1,26 @@
 #include "trap.h"
 #include "FLOAT.h"
-
+// FLOAT F_mul_F(FLOAT a, FLOAT b) {
+// 	long long c = (long long)a * (long long)b;
+// 	return (FLOAT)(c >> 16);
+// }
 // FLOAT f(FLOAT x) { 
 // 	/* f(x) = 1/(1+25x^2) */
 // 	return F_div_F(int2F(1), int2F(1) + F_mul_int(F_mul_F(x, x), 25));
 // }
 
-// FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
-// 	int k;
-// 	FLOAT s,h;
-// 	h = F_div_int((b - a), n);
-// 	s = F_div_int(fun(a) + fun(b), 2 );
-// 	for(k = 1; k < n; k ++) {
-// 		s += fun(a + F_mul_int(h, k));
-// 	}
-// 	s = F_mul_F(s, h);
-// 	return s;
-// }
-FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	long long c = (long long)a * (long long)b;
-	return (FLOAT)(c >> 16);
+FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
+	int k;
+	FLOAT s,h;
+	h = F_div_int((b - a), n);
+	s = F_div_int(fun(a) + fun(b), 2 );
+	for(k = 1; k < n; k ++) {
+		s += fun(a + F_mul_int(h, k));
+	}
+	s = F_mul_F(s, h);
+	return s;
 }
+
 int main() { 
 	// FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
 	// FLOAT ans = f2F(0.551222);
