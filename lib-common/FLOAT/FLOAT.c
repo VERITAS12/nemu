ab1416419ef57f3c8614ef590b6697e41d552606
tracer-ics2016 <tracer@njuics.org>
2020-09-28 08:36:41 +0000

    >  run 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:36:41 up 2 days, 23:31, 11 users,  load average: 0.74, 0.65, 0.55 caebb79fa53a0a027c159ab53b0d1151efa97fd6

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -1,8 +1,9 @@
 #include "FLOAT.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	nemu_assert(0);
-	return 0;
+	// nemu_assert(0);
+	long long result = 1ll * a * b;
+	return result>16;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
@@ -24,8 +25,28 @@ FLOAT F_div_F(FLOAT a, FLOAT b) {
 	 * out another way to perform the division.
 	 */
 
-	nemu_assert(0);
-	return 0;
+	// nemu_assert(0);
+	int sign = 1;
+	if(a < 0){
+		a = -a;
+		sign = -sign;
+	}
+	if(b < 0){
+		b = -b;
+		sign = -sign;
+	}
+	int result = a / b;
+	a = a % b;
+	int i = 0;
+	for(;i<16;i++){
+		result <<= 1;
+		a <<= 1;
+		if(a >= b){
+			a -= b;
+			result++;
+		}
+	}
+	return result * sign;
 }
 
 FLOAT f2F(float a) {
@@ -39,13 +60,23 @@ FLOAT f2F(float a) {
 	 * performing arithmetic operations on it directly?
 	 */
 
-	nemu_assert(0);
-	return 0;
+	// nemu_assert(0);
+	int tmp = *(int *)&a;
+	int sign = (tmp>>31);
+	int exp = ((tmp>>23)&0xff);
+	FLOAT result = tmp;
+	result &= 0x7fffff;
+	if(exp==0)return 0;
+	result |= (1 << 23);
+	exp -= 150;
+	if(exp>-16)result<<=exp+16;
+	else if(exp < -16)result >>= -16 - exp;
+	return sign?-result:result;
 }
 
 FLOAT Fabs(FLOAT a) {
-	nemu_assert(0);
-	return 0;
+	
+	return a < 0? -a :a;
 }
 
 /* Functions below are already implemented */
