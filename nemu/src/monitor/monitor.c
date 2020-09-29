6938fba8b26d9c3933bdca2db22195378a80b645
tracer-ics2016 <tracer@njuics.org>
2020-09-29 11:13:53 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:13:53 up 4 days,  2:09, 14 users,  load average: 0.56, 0.65, 0.62 dec368cd2587bb9a585b4be29be1898fb07828

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -87,8 +87,9 @@ void restart() {
 	/* Set the initial instruction pointer. */
 	cpu.eip = ENTRY_START;
 	//printf("START: %0x\n", cpu.eip);
-	cpu.CF = 1;
-	cpu.PF = cpu.ZF = cpu.SF=cpu.IF=cpu.DF=cpu.OF=0;
+	cpu.eflags = 0x00000002;
+	// cpu.CF = 1;
+	// cpu.PF = cpu.ZF = cpu.SF=cpu.IF=cpu.DF=cpu.OF=0;
 	/* Initialize DRAM. */
 	init_ddr3();
 }
