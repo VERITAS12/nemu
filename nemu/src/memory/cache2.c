b6d9c504c4c092f887b6d7ee55e15d7c4a9d9e7b
tracer-ics2016 <tracer@njuics.org>
2020-10-17 09:06:23 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:06:23 up 1 day,  6:12,  1 user,  load average: 0.97, 0.86, 0.65 45f1adf07817de679b797427c2fd56bcd3dcd168

--- nemu/src/memory/cache2.c
+++ nemu/src/memory/cache2.c
@@ -7,7 +7,7 @@
 uint32_t dram_read_64(hwaddr_t, void *);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
-#define GROUP_WIDTH 7
+#define GROUP_WIDTH 12
 #define BLOCK_WIDTH 6
 #define TAG_WIDTH (27 - GROUP_WIDTH - BLOCK_WIDTH)
 #define HW_MEM_SIZE (1 << (GROUP_WIDTH+BLOCK_WIDTH+TAG_WIDTH)) 
@@ -21,28 +21,28 @@ typedef union {
 } cache_addr;
 
 #define NR_GROUP (1 << GROUP_WIDTH)
-#define NR_ROW (1 << 3)
+#define NR_ROW (1 << 4)
 #define NR_BLOCK (1 << BLOCK_WIDTH)
 typedef struct cache{
 	struct{
-		uint32_t valid : 1;
+		uint32_t valid : 3;
 		uint32_t tag : 14;
 		uint8_t blocks[NR_BLOCK];
 	}row[NR_ROW];
 }L;
 
-L L1[NR_GROUP];
-void init_cache1(){
+L L2[NR_GROUP];
+void init_cache2(){
 	int i, j;
 	for(i = 0; i < NR_GROUP;i++){
 		for(j = 0; j < NR_ROW;i++){
-			L1[i].row[j].valid = 0;	
+			L2[i].row[j].valid = 0;	
 		}
 	}
 }
 
-static void cache1_read(hwaddr_t addr, void *data){
-	Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!(cache1_read)", addr);
+static void cache2_read(hwaddr_t addr, void *data){
+	Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!(cache2_read)", addr);
 	cache_addr temp;
 	temp.addr = addr & ~BURST_MASK;
 	uint32_t tag = temp.tag;
@@ -50,34 +50,34 @@ static void cache1_read(hwaddr_t addr, void *data){
 	uint32_t off = temp.off;
 	int i;
 	for(i = 0;i < NR_ROW; i++){
-		if(L1[group].row[i].tag != tag || L1[group].row[i].valid != 1)continue;
-		memcpy(data, L1[group].row[i].blocks+off, BURST_LEN);
+		if(L2[group].row[i].tag != tag || L2[group].row[i].valid != 1)continue;
+		memcpy(data, L2[group].row[i].blocks+off, BURST_LEN);
 		return;
 	}
 
 	int a;
 	srand((unsigned)time(NULL));
 	a = rand()%8;
-	dram_read_64(addr, L1[group].row[a].blocks);
-	L1[group].row[a].valid = 1;
-	L1[group].row[a].tag = tag;
-	memcpy(data, L1[group].row[a].blocks+off, BURST_LEN);
+	dram_read_64(addr, L2[group].row[a].blocks);
+	L2[group].row[a].valid = 1;
+	L2[group].row[a].tag = tag;
+	memcpy(data, L2[group].row[a].blocks+off, BURST_LEN);
 
 }
-uint32_t L1_read(hwaddr_t addr, size_t len){
+uint32_t L2_read(hwaddr_t addr, size_t len){
 	uint32_t offset = addr & BURST_MASK;
 	uint8_t temp[2 * BURST_LEN];
-	cache1_read(addr, temp);
+	cache2_read(addr, temp);
 	if(offset + len > BURST_LEN){
-		cache1_read(addr + BURST_LEN, temp + BURST_LEN);
+		cache2_read(addr + BURST_LEN, temp + BURST_LEN);
 	}
 	return unalign_rw(temp + offset, 4);
 }
 
 
 
-static void cache1_write(hwaddr_t addr, void *data, uint8_t *mask){
-	Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!(cache1_read)", addr);
+static void cache2_write(hwaddr_t addr, void *data, uint8_t *mask){
+	Assert(addr < HW_MEM_SIZE, "physical address %x is outside of the physical memory!(cache2_read)", addr);
 	cache_addr temp;
 	temp.addr = addr & ~BURST_MASK;
 	uint32_t tag = temp.tag;
@@ -85,14 +85,14 @@ static void cache1_write(hwaddr_t addr, void *data, uint8_t *mask){
 	uint32_t off = temp.off;
 	int i;
 	for(i = 0;i < NR_ROW; i++){
-		if(L1[group].row[i].tag != tag || L1[group].row[i].valid != 1)continue;
-		memcpy_with_mask(L1[group].row[i].blocks+off, data, BURST_LEN, mask);
+		if(L2[group].row[i].tag != tag || L2[group].row[i].valid != 2)continue;
+		memcpy_with_mask(L2[group].row[i].blocks+off, data, BURST_LEN, mask);
 		return;
 	}
 
 }
 
-void L1_write(hwaddr_t addr, size_t len, uint32_t data) {
+void L2_write(hwaddr_t addr, size_t len, uint32_t data) {
 	uint32_t offset = addr & BURST_MASK;
 	uint8_t temp[2 * BURST_LEN];
 	uint8_t mask[2 * BURST_LEN];
@@ -101,11 +101,11 @@ void L1_write(hwaddr_t addr, size_t len, uint32_t data) {
 	*(uint32_t *)(temp + offset) = data;
 	memset(mask + offset, 1, len);
 
-	cache1_write(addr, temp, mask);
+	cache2_write(addr, temp, mask);
 
 	if(offset + len > BURST_LEN) {
 		/* data cross the burst boundary */
-		cache1_write(addr + BURST_LEN, temp + BURST_LEN, mask + BURST_LEN);
+		cache2_write(addr + BURST_LEN, temp + BURST_LEN, mask + BURST_LEN);
 	}
 	dram_write(addr, len, data);
 }
