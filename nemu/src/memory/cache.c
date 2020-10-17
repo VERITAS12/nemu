859c747155f3deae16564cd0e5beada38e5542df
tracer-ics2016 <tracer@njuics.org>
2020-10-17 08:04:00 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:04:00 up 1 day,  5:10,  1 user,  load average: 0.54, 0.62, 0.62 6dc502b059f79e89541f561e1b1fa0b6e293245c

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -62,7 +62,7 @@ static void cache1_read(hwaddr_t addr, void *data){
 	srand((unsigned)time(NULL));
 	a = 0;
 	dram_read_64(addr, L1[group].row[a].blocks);
-	L1[group].row[a].valid = 1;
+	L1[group].row[a].valid = 0;
 	L1[group].row[a].tag = tag;
 	if(L1[group].row[a].tag != tag || L1[group].row[a].valid != 1)return;
 	memcpy(data, L1[group].row[a].blocks+off, BURST_LEN);
