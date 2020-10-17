e47152e991db6cc541db9ab49e59c135ef6587f3
tracer-ics2016 <tracer@njuics.org>
2020-10-17 08:52:00 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:52:00 up 1 day,  5:58,  1 user,  load average: 0.61, 0.54, 0.48 79254edf2e6c3644633939ade9dfbb8a9bc8666a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,8 +12,8 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	dram_write(addr, len, data);
 	L1_write(addr, len, data);
+	dram_write(addr, len, data);	
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
