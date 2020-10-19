854d23cbdfa5b5b0de17d4fd62959e30ac2a650f
tracer-ics2016 <tracer@njuics.org>
2020-10-19 08:26:37 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:26:37 up 3 days,  5:32,  1 user,  load average: 0.52, 0.32, 0.22 b4da98d479ab1e00a0eaa97e9f55287a53d3b4b1

--- lib-common/x86-inc/cpu.h
+++ lib-common/x86-inc/cpu.h
@@ -2,7 +2,7 @@
 #define __X86_INC_CPU_H__
 
 /* the Control Register 0 */
-typedef union CR0 {
+typedef union{
 	struct {
 		uint32_t protect_enable      : 1;
 		uint32_t monitor_coprocessor : 1;
@@ -21,7 +21,8 @@ typedef union CR0 {
 	};
 	uint32_t val;
 } CR0;
-CR0.val = 0;
+CR0 t;
+t.val = 0;
 /* the Control Register 3 (physical address of page directory) */
 typedef union CR3 {
 	struct {
