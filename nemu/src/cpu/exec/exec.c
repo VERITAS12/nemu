f02d41ad2689817936934956210cb6112bedb37a
tracer-ics2016 <tracer@njuics.org>
2020-10-19 09:31:08 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:31:08 up 3 days,  6:37,  1 user,  load average: 0.62, 0.24, 0.14 5f1a0509b14821e2f9590689a2a6475f252761a

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -84,11 +84,11 @@ make_group(group5,
 	jmp_rm_v, inv, push_rm_v, inv)
 
 make_group(group6,
-	inv, inv, lgdt, inv, 
+	inv, inv, inv, inv, 
 	inv, inv, inv, inv)
 
 make_group(group7,
-	inv, inv, inv, inv, 
+	inv, inv, lgdt, inv, 
 	inv, inv, inv, inv)
 
 
