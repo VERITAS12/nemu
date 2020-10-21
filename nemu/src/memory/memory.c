226dca651cf8a25b91cb9ef9371d928845c7baaa
tracer-ics2016 <tracer@njuics.org>
2020-10-21 14:34:50 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:34:50 up 5 days, 11:40,  1 user,  load average: 0.29, 0.16, 0.28 fa2226c8f48b309ded31de5c9fc06e8f65d0f617

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -75,8 +75,8 @@ void swaddr_write(swaddr_t addr, size_t len, uint32_t data, uint8_t sreg) {
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
-	lnaddr_t lnaddr = seg_translate(addr, len, sreg);
-	lnaddr_write(lnaddr, len, data);
+	//lnaddr_t lnaddr = seg_translate(addr, len, sreg);
+	lnaddr_write(addr, len, data);
 }
 void load_sr_cache(uint8_t sreg){
 	uint32_t addr = (cpu.SR[sreg].index << 3) + cpu.GDTR.base;
