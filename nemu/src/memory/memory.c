98a9d5c0bf104ef042c5b5639e2a974e5e8c9efd
tracer-ics2016 <tracer@njuics.org>
2020-10-22 11:11:58 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:11:58 up 6 days,  8:18,  1 user,  load average: 1.75, 1.26, 0.98 f71a1f22d2caa27dc05bd7e695d3cc933c5f976b

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -32,9 +32,7 @@ uint32_t page_translate(hwaddr_t addr){
 		paddr.val = addr;
 		//printf("get me\n");
 		bool flag = false;
-		uint32_t temp = check_TLB(addr, &flag);
-		if(flag);
-		else printf("no---------------------------------\n");		
+		uint32_t temp = check_TLB(addr, &flag);	
 		if(flag) return (temp << 12) + paddr.offset;
 		uint32_t base1 = (cpu.CR3.page_directory_base << 12) + (paddr.dir << 2);
 		PTE pte;
