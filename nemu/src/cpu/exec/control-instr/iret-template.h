b87979a504e97fde76b6218b034091235810553e
tracer-ics2016 <tracer@njuics.org>
2020-11-18 12:11:17 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:11:17 up 2 days,  8:35,  1 user,  load average: 0.50, 0.19, 0.10 7dd424949c0d35bb8251afd1c6bb42b36561b58f

--- nemu/src/cpu/exec/control-instr/iret-template.h
+++ nemu/src/cpu/exec/control-instr/iret-template.h
@@ -3,9 +3,10 @@
 #define instr iret
 
 make_helper(concat(iret_, SUFFIX)){
-	cpu.eflags = swaddr_read(reg_l(R_ESP), 4, R_SS), reg_l(R_ESP) += 4;
-	cpu.SR[R_CS].val = swaddr_read(reg_l(R_ESP), 4, R_SS), reg_l(R_ESP) += 4;
 	cpu.eip = swaddr_read(reg_l(R_ESP), 4, R_SS), reg_l(R_ESP) += 4;
+	cpu.SR[R_CS].val = swaddr_read(reg_l(R_ESP), 4, R_SS), reg_l(R_ESP) += 4;
+	load_sr_cache(R_CS);
+	cpu.eflags = swaddr_read(reg_l(R_ESP), 4, R_SS), reg_l(R_ESP) += 4;	
 	print_asm("iret");	
 	return 1;
 }
