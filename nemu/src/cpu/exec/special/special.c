9a460c0d662f113437c7e886d87935a5d76e233b
tracer-ics2016 <tracer@njuics.org>
2020-11-18 13:35:41 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:35:41 up 2 days,  9:59,  1 user,  load average: 0.64, 0.40, 0.28 bd8de4d8b8f30a72afaa8f5a347599e38210a57b

--- nemu/src/cpu/exec/special/special.c
+++ nemu/src/cpu/exec/special/special.c
@@ -28,7 +28,7 @@ char * to_str(uint32_t begin, uint32_t len){
 	int i = 0;	
 	for(;i<len;i++){
 		str[i] = swaddr_read(begin+i, 1, R_SS);
-		putchar(str[i]);
+		if(i!=len-1)putchar(str[i]);
 	}
 	str[len] = 0x00;
 	return str;
